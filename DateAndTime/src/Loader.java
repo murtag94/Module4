79e03bd17ea327c3aab99d0a21f8480557d5ce2e
Vlad <vlad.bar94@mail.ru>
2018-11-19 14:47:11 +0300

    Изменение в DateAndTime

--- DateAndTime/src/Loader.java
+++ DateAndTime/src/Loader.java
@@ -13,18 +13,25 @@ public class Loader
     public static void main(String[] args) throws IOException {
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EE", Locale.forLanguageTag("ENGLISH"));
         Calendar calendar = Calendar.getInstance();
+        Calendar calendarCurrent = Calendar.getInstance();
+        Date date;
 
+        System.out.println("Введите дату рождения");
         String fullDate = (new BufferedReader(new InputStreamReader(System.in))).readLine();
 
         if(!Pattern.compile("([0-2][0-9]|3[0-1])[/.](0[1-9]|1[0-2])[/.](18|19|20)[0-9]{2}").matcher(fullDate).matches())
-        calendar.set(1994, Calendar.JANUARY, 19);
-        Calendar calendarCurrent = Calendar.getInstance();
-        Date date;
-        for(int i = 0; calendar.before(calendarCurrent); i++)
         {
-            date = calendar.getTime();
-            System.out.println(i + " - " + dateFormat.format(date));
-            calendar.add(Calendar.YEAR, 1);
+            System.out.println("Неверно введена дата");
+        }
+        else{
+            String[] dateArray = fullDate.split("[/.]");
+            calendar.set(Integer.parseInt(dateArray[2]), Integer.parseInt(dateArray[1]) - 1, Integer.parseInt(dateArray[0]));
+            for(int i = 0; calendar.before(calendarCurrent); i++)
+            {
+                date = calendar.getTime();
+                System.out.println(i + " - " + dateFormat.format(date));
+                calendar.add(Calendar.YEAR, 1);
+            }
         }
     }
 }
